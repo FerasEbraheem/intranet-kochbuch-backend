c545e1f090eaa14575cae4fa587346e5fd426c77
ferasebraheem <feras.kh.ebraheem@gmail.com>
2025-06-04 09:47:54 +0000

    chore: configure Jest to collect code coverage and exclude test files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 export default {
   testEnvironment: 'node',
   transform: {},
-};
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.js",          
+    "!src/**/__tests__/**", 
+    "!src/db/init.js",
+    "!**/node_modules/**"       
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"]
+}
